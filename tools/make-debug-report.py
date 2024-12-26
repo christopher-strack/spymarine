90c74c4acfb4ee8ff58c4082ee6f9da57be06e70
Christopher Strack <christopher.strack@gmail.com>
2024-12-26 22:24:12 +0100

    Introduce make-debug-report.py

---
+++ tools/make-debug-report.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import asyncio
+import logging
+
+import spymarine
+
+
+async def main():
+    logging.basicConfig(
+        format="%(levelname)s:%(message)s",
+        level=logging.DEBUG,
+        filename="debug.log",
+        filemode="w",
+    )
+
+    print("Start creating a debug report in debug.log")
+
+    async with spymarine.DeviceReader() as reader:
+        await reader.read_sensors()
+
+    print("Finished")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
